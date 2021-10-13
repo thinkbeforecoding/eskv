1bfd0c095e22d73fc9a38767002bd72935bfdbfa
jchassaing <jeremie.chassaing@gmail.com>
2021-10-13 16:45:10 +0200

    Use dist in root dir

--- build.sh
+++ build.sh
@@ -10,5 +10,5 @@ npm ci  --prefix ./src/eskv.ui/
 
 
 dotnet fable ./src/eskv.ui/eskv.ui.fsproj  --noRestore
-npm exec --prefix ./src/eskv.ui/ parcel -- build ./src/eskv.ui/App.fs.js ./src/eskv.ui/style.scss
+npm exec -y --prefix ./src/eskv.ui/ parcel -- build ./src/eskv.ui/App.fs.js ./src/eskv.ui/style.scss --dist-dir ./dist
 dotnet pack -c Release -o bin/nuget --no-restore
